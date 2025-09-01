fbc34b4c410a69af3e5ba4baeb2c950315b5bb16
JIEUN <163732805+jxieun@users.noreply.github.com>
2025-09-01 09:45:49 +0900

    default props

---
+++ 2reactjs/reacthello-vite/src/App.jsx
@@ -0,0 +1,12 @@
+import MyComponent from "./components/MyComponent";
+
+const App = () => {
+  return (
+    <>
+      <MyComponent name="ReactJS" />
+      <MyComponent />
+    </>
+  );
+};
+
+export default App;
