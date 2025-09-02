458c71c3445e3a9fb6ba8b40ab7c76308670cafb
JIEUN <163732805+jxieun@users.noreply.github.com>
2025-09-02 14:51:59 +0900

    Initial commit with 0html_css_js, 1ecma_script, 2reactjs

--- 2reactjs/redux-counter-app/src/components/Counter.js
+++ 2reactjs/redux-counter-app/src/components/Counter.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Counter.css';
+import { decrement } from '../actions';
+
 const Counter = ({ number, color, onIncrement,
-    onDecrement, onSetColor }) => {
+    decrement, onSetColor }) => {
     return (
         <div
             className="Counter"
             onClick={onIncrement}
             onContextMenu={(e) => {
                 e.preventDefault();
-                onDecrement();
+                decrement();
             }}
             onDoubleClick={onSetColor}
             style={{ backgroundColor: color }}>
@@ -21,14 +23,14 @@ Counter.propTypes = {
     number: PropTypes.number,
     color: PropTypes.string,
     onIncrement: PropTypes.func,
-    onDecrement: PropTypes.func,
+    decrement: PropTypes.func,
     onSetColor: PropTypes.func
 };
 Counter.defaultProps = {
     number: 0,
     color: 'blue',
     onIncrement: () => console.warn('onIncrement not defined'),
-    onDecrement: () => console.warn('onDecrement not defined'),
+    decrement: () => console.warn('decrement not defined'),
     onSetColor: () => console.warn('onSetColor not defined')
 };
 export default Counter;
